181c9f8557691cc7d496dc027a19c2ceca9f76a2
hanaum <hanau12@gmail.com>
2016-07-03 15:18:55 -0700

    Update outdated dependencies.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
     basePath: '',
     frameworks: ['jasmine'],
     files: [{pattern: './config/karma-test-shim.js', watched: false}],
-    preprocessors: {'./config/karma-test-shim.js': ['webpackStream', 'sourcemap']},
+    preprocessors: {'./config/karma-test-shim.js': ['webpack', 'sourcemap']},
     webpack: webpackConfig,
     webpackMiddleware: {stats: 'errors-only'},
     webpackServer: {noInfo: true},
