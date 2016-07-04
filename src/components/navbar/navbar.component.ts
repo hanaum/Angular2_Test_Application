30ee200c0c5ed02f422e44c7a1627c75c126b67b
hanaum <hanau12@gmail.com>
2016-07-03 23:32:34 -0700

    Update clang-format

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'nav-bar',
   template: require('./nav.component.html'),
-  styles: [require('./nav.component.css')]
+  styles: [require('./navbar.component.css')]
 })
 
 export class NavComponent {
