bb9ac81704dd080014aa84888c85e870c3ea5280
hanaum <hanau12@gmail.com>
2016-07-03 19:30:34 -0700

    Separate nav bar into its own component.

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -2,12 +2,13 @@ import '../../../public/css/styles.css';
 
 import {Component} from '@angular/core';
 import {TasklistComponent} from '../tasklist/tasklist.component';
+import {NavComponent} from '../nav/nav.component';
 
 @Component({
   selector: 'my-app',
   template: require('./app.component.html'),
   styles: [require('./app.component.css')],
-  directives: [<any>TasklistComponent]
+  directives: [<any>TasklistComponent, <any>NavComponent]
 })
 
 export class AppComponent {
---
+++ src/components/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import '../../../public/css/styles.css';
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  template: require('./nav.component.html'),
+  styles: [require('./nav.component.css')]
+})
+
+export class NavComponent {
+}
