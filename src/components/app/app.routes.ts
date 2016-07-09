964d7b73e10ce88ab879298442697179f259bdd8
Hana Um <hanaum@users.noreply.github.com>
2016-07-08 17:34:03 -0700

    Revert "Edit list name"

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -3,18 +3,9 @@ import {RouterConfig, provideRouter} from '@angular/router';
 import {HomepageComponent} from '../homepage/homepage.component';
 import {TaskListViewComponent} from '../taskListView/taskListView.component';
 
-const DEFAULT_ROUTE: string = '';
-const TASKLIST_ROUTE: string = 'list/:id';
-
 const routes: RouterConfig = [
-  {
-    path: DEFAULT_ROUTE,
-    component: HomepageComponent as any
-  },
-  {
-    path: TASKLIST_ROUTE,
-    component: TaskListViewComponent as any
-  }
+  {path: '', component: HomepageComponent as any},
+  {path: 'list/:id', component: TaskListViewComponent as any}
 ];
 
 export const APP_ROUTER_PROVIDERS = [provideRouter(routes)];
--- src/components/homepage/homepage.component.ts
+++ src/components/homepage/homepage.component.ts
@@ -2,25 +2,22 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Subscription} from 'rxjs/Rx';
 
-import {AuthenticationService, AuthenticationState} from '../../services/authentication.service';
+import {AuthenticationService} from '../../services/authentication.service';
 import {TaskListService} from '../../services/taskList.service';
-
-const TASKLIST_ROUTE: string = '/list';
 
 @Component({
   selector: 'homepage',
   template: require('./homepage.component.html'),
   styles: [require('./homepage.component.css')],
-  providers: [TaskListService]
+  providers: [AuthenticationService, TaskListService]
 })
 
 /**
  * AppComponent serves as main component that holds base components.
  */
 export class HomepageComponent implements OnInit {
-  // Copy of AuthenticationState to use in HTML.
-  private authenticationStateReference = AuthenticationState;  // tslint:disable-line
-  private loggedIn: AuthenticationState = AuthenticationState.Unknown;
+  // 0 is unknown. 1 is logged in. -1 is not logged in.
+  private loggedIn: number = 0;
   private loginSubscription: Subscription;
 
   constructor(
@@ -30,14 +27,15 @@ export class HomepageComponent implements OnInit {
 
   ngOnInit() {
     this.loginSubscription =
-        this.authenticationService.loginState$.subscribe((state) => { this.loggedIn = state; });
+        this.authenticationService.loginState$.subscribe((state) => { this.loggedIn = state; })
   }
 
   ngOnDestroy() { this.loginSubscription.unsubscribe(); }
 
   getListId() {
-    let id = this.taskListService.getNewTaskListId(this.authenticationService.getUid());
-    this.router.navigate([TASKLIST_ROUTE, id]);
+    let id = this.taskListService.getNewTaskListId();
+    let link = ['/list', id];
+    this.router.navigate(link);
   }
 
   login() { this.authenticationService.login(); }
