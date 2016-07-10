83deb8d1b58c58b3a030046ef3a6ac32890b9e17
hanaum <hanau12@gmail.com>
2016-07-09 23:36:54 -0700

    Remove navbar logic from homepage.

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -2,11 +2,12 @@ import '../../../public/css/styles.css';
 
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
+import {NavbarComponent} from '../navbar/navbar.component';
 
 @Component({
   selector: 'my-app',
   template: require('./app.component.html'),
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, NavbarComponent as any]
 })
 
 /**
--- src/components/taskListView/taskListView.component.ts
+++ src/components/taskListView/taskListView.component.ts
@@ -2,14 +2,13 @@ import '../../../public/css/styles.css';
 
 import {Component} from '@angular/core';
 
-import {NavbarComponent} from '../navbar/navbar.component';
 import {TaskListComponent} from '../taskList/taskList.component';
 
 @Component({
   selector: 'task-list-view',
   template: require('./taskListView.component.html'),
   styles: [require('./taskListView.component.css')],
-  directives: [TaskListComponent as any, NavbarComponent as any]
+  directives: [TaskListComponent as any]
 })
 
 /**
