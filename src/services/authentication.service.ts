dc6341d524345216f3ee5d4877a5c8a118179970
hanaum <hanau12@gmail.com>
2016-07-10 01:01:41 -0700

    Change AngularFire dependency to FirebaseAuth in AuthenticationService.

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {AngularFire, FirebaseAuthState} from 'angularfire2/angularfire2';
+import {FirebaseAuth, FirebaseAuthState} from 'angularfire2/angularfire2';
 import {BehaviorSubject, Observable} from 'rxjs/Rx';
 
 export enum AuthenticationState {
@@ -15,16 +15,16 @@ export class AuthenticationService {
   private authState: FirebaseAuthState;
   public loginState$: Observable<AuthenticationState> = this.loginState.asObservable();
 
-  constructor(private af: AngularFire) { this.subscribeToAuth(); }
+  constructor(private firebaseAuth: FirebaseAuth) { this.subscribeToAuth(); }
 
   public getUserId(): string { return this.authState == null ? null : this.authState.uid; }
 
-  public login() { this.af.auth.login(); }
+  public login() { this.firebaseAuth.login(); }
 
-  public logout() { this.af.auth.logout(); }
+  public logout() { this.firebaseAuth.logout(); }
 
   private subscribeToAuth() {
-    this.af.auth.subscribe((auth) => {
+    this.firebaseAuth.subscribe((auth) => {
       this.authState = auth;
       if (this.authState == null) {
         this.loginState.next(AuthenticationState.LOGGED_OUT);
