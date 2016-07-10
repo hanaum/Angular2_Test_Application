ccdb77cefcf8665de7b40be247a1ba98798300a6
hanaum <hanau12@gmail.com>
2016-07-10 13:38:37 -0700

    Add ng-bootstrap.

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,13 +1,15 @@
 import '../../../public/css/styles.css';
 
 import {Component} from '@angular/core';
+import {NGB_DIRECTIVES, NGB_PRECOMPILE} from '@ng-bootstrap/ng-bootstrap';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {NavbarComponent} from '../navbar/navbar.component';
 
 @Component({
   selector: 'my-app',
   template: require('./app.component.html'),
-  directives: [ROUTER_DIRECTIVES, NavbarComponent as any]
+  directives: [ROUTER_DIRECTIVES, NGB_DIRECTIVES, NavbarComponent as any],
+  precompile: [ NGB_PRECOMPILE ]
 })
 
 /**
