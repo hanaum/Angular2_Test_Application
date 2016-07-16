d4ccdf2d6e96064114901e117cd2751865df9274
hanaum <hanau12@gmail.com>
2016-07-16 16:06:39 -0700

    Attach autocomplete to addTask name box.

--- src/components/addTask/addTask.component.ts
+++ src/components/addTask/addTask.component.ts
@@ -1,6 +1,8 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Output, OnInit} from '@angular/core';
 
 import {TaskItem} from '../../services/taskItem';
+
+declare const google: any;
 
 @Component({
   selector: 'add-task',
@@ -11,11 +13,17 @@ import {TaskItem} from '../../services/taskItem';
 /**
  * TaskListComponent renders the table containing a list of TaskItems.
  */
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
   @Output() taskEmitter = new EventEmitter<TaskItem>();
 
   private model = new TaskItem('', 3);
   private hidden = false;
+
+  ngOnInit() {
+    // TODO investigate using @viewChild.
+    let taskNameInputBox = document.getElementById('add-task-name');
+    let autoComplete = new google.maps.places.Autocomplete(taskNameInputBox);
+  }
 
   onSubmit() {
     this.taskEmitter.emit(this.model);
